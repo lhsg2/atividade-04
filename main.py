7b05d21d627af4f66cceb612b42ff1d927997ccc
Luiz Henrique <138501027+lhsg2@users.noreply.github.com>
2024-06-22 11:43:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+money = float(input("degite o valor de dinheiro ganhado por hora:"))
+time = float(input("degite quantas horas trabalhou no mes:"))
+salario = money*time
+print(f"salario:{salario}")
